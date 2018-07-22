66625738ec9b5b4a7994a71cf0bf7c25e94cfed9
Anton Balbido <antonbalbido@antons-imac.attlocal.net>
2018-07-21 18:53:27 -0700

    Added seven case

---
+++ Part03/Seven.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Seven {
+  public static void main(String[] args) {
+    String something0;
+    String something1;
+    Scanner otherthing;
+
+    otherthing = new Scanner(System.in);
+
+    System.out.println("type words then press enter:");
+    something0 = otherthing.nextLine();
+    System.out.println("you said " + something0);
+
+    System.out.println("type words then press enter:");
+    something1 = otherthing.nextLine();
+    System.out.println("you said " + something1);
+
+    something0 = something1 + something0;
+
+    System.out.println("type words then press enter:");
+    something1 = otherthing.nextLine();
+    System.out.println("you said " + something1);
+
+    System.out.println("you said " + something1 + something0 + something1);
+  }
+}
---
+++ Part03/Seven.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+javac Seven.java
+java Seven
+rm Seven.class
